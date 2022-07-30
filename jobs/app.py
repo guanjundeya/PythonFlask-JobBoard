ae7b4e649992a64fc9335eadd4db9b7ff75d8ce7
unknown <tangyafan01@gmail.com.com>
2022-07-30 16:44:47 -0700

    bug fix

--- jobs/app.py
+++ jobs/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/secret_page')
+@app.route('/jobs')
 def jobs():
     return render_template('index.html')
 
